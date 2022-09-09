9571dd70048d06fb9f8682004bcf3505195c6e22
Codesait <melasin4@gmail.com>
2022-09-09 17:16:18 +0100

    modified splash screen viewmodel and created an empty home screen for splash navigation

---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeSreen extends StatelessWidget {
+  const HomeSreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
--- lib/utils/navigator.dart
+++ lib/utils/navigator.dart
@@ -2,27 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:hi_beat/app.dart';
 import 'package:hi_beat/src/utils.dart';
 
-
 class Nav {
   static const className = 'Navigtor';
 
-  static void popTo( Widget page) {
+  /// >  [popTo] pops the current page off the stack and replaces it with the page passed in
+  ///
+  /// Args:
+  ///   page (Widget): The page to pop to.
+  static void popTo(Widget page) {
+    ///* A function that pops the current page off the stack and replaces it with the page passed in.
+    ///* and Logging the action to the console.
+    getLogger(className).d('POPPED TO: $page');
     Navigator.pushReplacement(
       navigatorKey.currentContext!,
       MaterialPageRoute<void>(
         builder: (BuildContext context) => page,
       ),
-    ).whenComplete(() => getLogger(className).v('POPPED TO: $page'));
+    );
   }
 
-  static void goTo(dynamic context, Widget page) {
+  /// [goTo] is a function that takes a `Widget` as a parameter and navigates to that page
+  ///
+  /// Args:
+  ///   page (Widget): The page to navigate to.
+  static void goTo(Widget page) {
     Navigator.push(
-      context,
+      navigatorKey.currentContext!,
       MaterialPageRoute<void>(
         builder: (BuildContext context) => page,
       ),
-    ).whenComplete(() => getLogger(className).v('NAVIGATING TO: $page'));
+    ).then((v) => getLogger(className).d('NAVIGATING TO: $page'));
   }
-
-
 }
