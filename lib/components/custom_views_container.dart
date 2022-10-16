54dbe5fd704527fd768663f1ff67b9020e33b52a
Codesait <melasin4@gmail.com>
2022-10-16 23:51:11 +0100

    local playlist ui

--- lib/components/custom_views_container.dart
+++ lib/components/custom_views_container.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ViewsParentContainer extends StatelessWidget {
-  const ViewsParentContainer(
-      {super.key,
-      required this.child,
-      this.padding,
-      this.alignment,
-      this.width,
-      this.height});
+  const ViewsParentContainer({
+    super.key,
+    required this.child,
+    this.padding,
+    this.margin,
+    this.alignment,
+    this.width,
+    this.height,
+  });
 
   final Widget child;
   final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
   final AlignmentGeometry? alignment;
   final double? height, width;
 
@@ -20,6 +23,7 @@ class ViewsParentContainer extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(color: Theme.of(context).cardColor),
         padding: padding,
+        margin: margin,
         alignment: alignment,
         height: height,
         width: width,
--- lib/screens/myLibrary/widgets/my_playlist.dart
+++ lib/screens/myLibrary/widgets/my_playlist.dart
@@ -1,16 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:hi_beat/src/components.dart';
+import 'package:hi_beat/src/res.dart';
 
 class MyPlayLists extends StatelessWidget {
   const MyPlayLists({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ViewsParentContainer(
-      alignment: Alignment.center,
-      child: Text(
-        'My playlists',
-        style: TextStyle(),
+    return ViewsParentContainer(
+      padding: const EdgeInsets.only(
+        bottom: 190,
+      ),
+      child: ListView(
+        children: const [
+          // create playlist button
+          CreatePlaylist(),
+
+          // divider seperating create widget and list
+          Divider(
+            thickness: 0.4,
+            indent: 20,
+            endIndent: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CreatePlaylist extends StatelessWidget {
+  const CreatePlaylist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ListTile(
+      onTap: () {},
+      minVerticalPadding: 30,
+      title: Text(
+        'New Playlist',
+        style: theme.textTheme.titleSmall,
+      ),
+      leading: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: AppColors.primary,
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: const Center(
+          child: Icon(
+            Icons.add,
+            color: AppColors.white,
+          ),
+        ),
       ),
     );
   }
